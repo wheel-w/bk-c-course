5f22ab4d9c55271777a293510ceaa237d24ee9a6
“黄渭涵“ <1245403676@qq.com>
2021-12-10 21:36:37 +0800

    feature:course

--- course/models.py
+++ course/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    course_name = models.CharField("课程名称", max_length=90, blank=True)
-    course_introduction = models.TextField("课程简介", blank=True)
+    course_name = models.CharField("课程名称", max_length=90)
+    course_introduction = models.TextField("课程简介", blank=True, null=True)
     teacher = models.CharField("教师姓名", max_length=90)
     create_people = models.CharField("创建人", max_length=90)
     manage_student = models.TextField("学生管理员", null=True, blank=True)
