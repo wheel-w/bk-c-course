c3df8c4ac68a49bcf371422c58cc5655b8635205
计元杰 <3281813461@qq.com>
2021-12-11 14:10:06 +0000

    !25 成员管理表单 * 新增成员表modle

--- course/models.py
+++ course/models.py
@@ -13,3 +13,39 @@ class Course(models.Model):
 class UserCourseContact(models.Model):
     user_id = models.IntegerField("用户id")
     course_id = models.IntegerField("课程id")
+
+
+class Member(models.Model):
+    class Gender:
+        MAN = "MAN"
+        WOMAN = "WOMAN"
+
+    class Identity:
+        TEACHER = "TEACHER"
+        STUDENT = "STUDENT"
+        NOT_CERTIFIED = "NOT_CERTIFIED"
+
+    GENDER = [
+        (Gender.MAN, "男"),
+        (Gender.WOMAN, "女")
+    ]
+    IDENTITY = [
+        (Identity.TEACHER, "老师"),
+        (Identity.STUDENT, "学生"),
+        (Identity.NOT_CERTIFIED, "未认证")
+    ]
+
+    username = models.CharField("saas用户名", max_length=50)
+    openid = models.CharField("wechat唯一标识", max_length=50, blank=True, null=True)
+    class_number = models.CharField("学号/工号", max_length=30, blank=True, null=True)
+    name = models.CharField("姓名", max_length=30)
+
+    professional_class = models.CharField("专业班级", max_length=30, blank=True, null=True)
+
+    gender = models.CharField(max_length=20, choices=GENDER,)
+    identity = models.CharField(max_length=20, choices=IDENTITY, default=Identity.NOT_CERTIFIED)
+
+    phone_number = models.CharField("手机号", max_length=30, blank=True, null=True)
+    email_number = models.EmailField("邮箱", max_length=50, blank=True, null=True)
+    qq_number = models.CharField("QQ号", max_length=30, blank=True, null=True)
+    wechat_number = models.CharField("微信号", max_length=50, blank=True, null=True)
