2ea64cda63f5f9b595ceed6c7cde3614641dabc6
陈梓健 <9950318+yuqinsanx@user.noreply.gitee.com>
2021-12-28 02:28:32 +0000

    !45 注册登录功能 * feature:登录注册功能

--- frontend/applets/app.js
+++ frontend/applets/app.js
@@ -1,19 +1,54 @@
-// app.js
 App({
+  globalData: {
+  },
   onLaunch() {
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if(res.code){
+          // 将code发送到后端
+          wx.request({
+            url: 'http://dev.paas-edu.bktencent.com:8000/weixin/login',
+            data: {
+              code: res.code,
+            },
+            header: {
+              'content-type': 'application/json' // 默认值
+            },
+            success(res){
+              if(res.data.result){
+                let state = res.data.state;
+                wx.setStorageSync('states', state)
+               wx.setStorage({
+                key:'username',
+                data: res.data.username,
+              })
+              wx.switchTab({
+                url: '/pages/home/home',
+              })
+              }
+              else{
+                let state = res.data.data.state;
+                wx.setStorageSync('states', state)
+                wx.navigateTo({
+                  url: '../register/register',
+                })
+              }
+            }
+          })
+          
+        }
+        else{
+          wx.showToast({
+            icon:'none',
+            title: '网络错误',
+          })
+        }
       }
     })
   },
-  globalData: {
-    userInfo: null
-  }
 })
---
+++ frontend/applets/pages/home/home.js
@@ -0,0 +1,12 @@
+Page({
+  goDanxuan(){
+    wx.navigateTo({
+      url: '/pages/danxuan/danxuan',
+    })
+  },
+  goDuoxuan(){
+    wx.navigateTo({
+      url: '/pages/duoxuan/duoxuan',
+    })
+  }
+})
