3f6169cca2de17b41945798f99267783b099c6c2
杨硕 <2677588694@qq.com>
2022-01-19 22:13:57 +0800

    分页器返回格式

--- course/views.py
+++ course/views.py
@@ -455,7 +455,7 @@ def search_course_student(request):
                 "code": 200,
                 "data": page_info_list,  # 当前页数据
                 "page": int(page),  # 这是是返回当前页码给前端
-                "page_range": list(paginator.page_range),  # 这个参数是告诉前端一共有多少页
+                "count": len(student_list),
             },
             json_dumps_params={"ensure_ascii": False},
         )
