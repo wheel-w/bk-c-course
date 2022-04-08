aa7006fe0e98d6028ba4b65bd845e3fe74c3414a
BabyMuu <1630640@qq.com>
2022-04-08 11:15:59 +0800

    feature:建立用户, 用户标签, 用户-标签关联模型

---
+++ userManager/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import User, UserTag, UserTagContact
+
+
+# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('u_id', 'name', 'gender', 'phone_number')
+
+
+class UserTagAdmin(admin.ModelAdmin):
+    list_filter = ('tag_value', 'tag_color', 'is_built_in', 'sub_project')
+
+
+class UserTagContactAdmin(admin.ModelAdmin):
+    list_filter = ('id', 'user_id', 'tag_id')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(UserTag, UserTagAdmin)
+admin.site.register(UserTagContact, UserTagContactAdmin)
