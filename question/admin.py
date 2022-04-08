78033fa69198acc71c84d6d8b262667eed4cd0a4
Mocus EZ <285918468@qq.com>
2022-04-08 22:34:01 +0800

    feature: enable questions at admin

--- question/admin.py
+++ question/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Question, QuestionTag, QuestionTagContact
+
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(QuestionTag)
+admin.site.register(QuestionTagContact)
