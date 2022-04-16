3ed6f8a33495c4220a8fe16ef0e43a309b5af97f
BabyMuu <1630640@qq.com>
2022-04-16 13:23:25 +0800

    feature: 1,将批量操作放入batch视图中; 2,user添加根据tag_value进行筛选; 3, 添加了一些必要的注释

--- user_manager/views.py
+++ user_manager/views.py
@@ -33,7 +33,7 @@ class AccountView(GenericViewSet):
     """用户登录之后自动获取用户信息"""
 
     queryset = Account.objects.all()
-    serializer_class = serialize.AccountDeleteSerializer
+    serializer_class = serialize.AccountGetSerializer
 
     def retrieve(self, request, *args, **kwargs):
         """进入页面后获取用户信息, 如果没有则自动创建一个将username当做name的user"""
@@ -68,16 +68,24 @@ class OriginAccountView(ViewSet):
         data = json.loads(data.content)
         return Response(data["data"])
 
+
+class BatchView(ViewSet):
+    queryset = User.objects.all()
+
     @action(methods=["POST"], detail=False)
-    def batch_create(self, request, *args, **kwargs):
+    def add(self, request, *args, **kwargs):
         """批量增加用户"""
+        # 参数校验
         usernames = request.data.get("usernames")
         if not usernames or not isinstance(usernames, list):
             return Response("请传入一个用户名列表", exception=True)
         # 获取已经存在的用户, 并在usernames列表中删除这些用户
-        accounts = self.queryset.filter(username__in=usernames).values("username")
+        accounts = Account.objects.filter(username__in=usernames).values("username")
+
+        exist_accounts = []
         for account in accounts:
             usernames.remove(account["username"])
+            exist_accounts.append(account["username"])
         if not usernames:
             return Response("所选用户已经添加到本系统", exception=True)
         # 批量增加 Account
@@ -94,12 +102,43 @@ class OriginAccountView(ViewSet):
             )
         Account.objects.bulk_create(account_list)
         # 批量增加 User
-        account_ids = Account.objects.filter(username__in=usernames).values("id")
+        account_ids = Account.objects.filter(username__in=usernames).values(
+            "username", "id"
+        )
         user_list = [
-            User(id=account["id"], account_id=account["id"]) for account in account_ids
+            User(id=account["id"], account_id=account["id"], name=account["username"])
+            for account in account_ids
         ]
         User.objects.bulk_create(user_list)
-        return Response("ok")
+        return Response({"existent": exist_accounts, "add": usernames})
+
+    @action(methods=["POST"], detail=False)
+    def delete(self, request, *args, **kwargs):
+        """批量删除User"""
+        # 参数校验
+        id_list = request.data.get("id_list")
+        if not id_list or not isinstance(id_list, list):
+            return Response("请传入一个id列表", exception=True)
+        # 获取要删除的用户列表
+        queryset = self.queryset.filter(id__in=id_list)
+        delete_users = []
+        for i in queryset:
+            # 纪录删除的用户姓名
+            delete_users.append(i.name)
+            # 在删除列表中删除已经删除的用户名
+            id_list.remove(i.id)
+        if not queryset:
+            return Response("没有找到任何对应用户", exception=True)
+        # 统一删除
+        queryset.delete()
+        return Response(
+            {
+                "delete_count": len(delete_users),
+                "delete": delete_users,
+                "not_found": id_list,
+            },
+            status=status.HTTP_204_NO_CONTENT,
+        )
 
 
 class UserView(GenericViewSet, DestroyModelMixin):
@@ -112,19 +151,39 @@ class UserView(GenericViewSet, DestroyModelMixin):
     filter_fields = ["name", "gender"]
 
     def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
+        """获取用户信息, 并返回"""
+        queryset = self.get_queryset()
+        # 根据 tag_value 筛选
+        role = request.query_params.get("role")
+        if role:
+            if isinstance(role, list):
+                role_ids = UserTag.objects.filter(tag_value__in=role).values("id")
+            elif isinstance(role, str):
+                role_ids = UserTag.objects.filter(tag_value=role).values("id")
+            else:
+                Response("请返回一个tag_value列表或者一个tag_value", exception=True)
+            user_ids = (
+                UserTagContact.objects.filter(tag_id__in=role_ids)
+                .values("user_id")
+                .distinct()
+            )
+            queryset = queryset.filter(id__in=user_ids)
+        # 根据 filter_class 进行筛选
+        queryset = self.filter_queryset(queryset)
+        # 分页
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
-            # 在返回的字段中添加标签(与 listMixin 的唯一区别)
+            # 在返回的字段中添加tag字段(与 listMixin 的唯一区别)
             self.add_tag(serializer.data)
             return self.get_paginated_response(serializer.data)
-
+        # 序列化信息
         serializer = self.get_serializer(queryset, many=True)
         self.add_tag(serializer.data)
         return Response(serializer.data)
 
     def add_tag(self, users):
+        """给每一个返回的 User 增加一个tag字段"""
         user_ids = [user.get("id") for user in users]
         user_tag_dic = self.get_user_tag_map(user_ids)
         for user in users:
@@ -137,6 +196,7 @@ class UserView(GenericViewSet, DestroyModelMixin):
 
     @staticmethod
     def get_user_tag_map(user_ids):
+        """获取每一个返回的 User 所拥有的标签"""
         tag_conns = UserTagContact.objects.filter(user_id__in=user_ids)
         tag_ids = {tag_conn.tag_id for tag_conn in tag_conns}
         tags = UserTag.objects.filter(id__in=tag_ids)
@@ -161,18 +221,6 @@ class UserView(GenericViewSet, DestroyModelMixin):
         data = dict(serializer.data)
         data["tag"] = self.get_user_tag_map([data.get("id")]).pop(instance.id)
         return Response(data)
-
-    @action(methods=["POST"], detail=False)
-    def batch_delete(self, request, *args, **kwargs):
-        """批量删除"""
-        id_list = request.data.get("id_list")
-        if not id_list or not isinstance(id_list, list):
-            return Response("请传入一个id列表", exception=True)
-        queryset = User.objects.filter(id__in=id_list)
-        if not queryset:
-            return Response("没有找到任何对应用户", exception=True)
-        queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class UserUpdateView(GenericViewSet, UpdateModelMixin):
