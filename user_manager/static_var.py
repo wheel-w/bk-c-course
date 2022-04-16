7b29b55276c348d0688536a80af5954c76af83b4
BabyMuu <1630640@qq.com>
2022-04-16 14:51:15 +0800

    feature: 将敏感信息放入全局变量

--- user_manager/static_var.py
+++ user_manager/static_var.py
@@ -10,18 +10,15 @@ Unless required by applicable Law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific Language governing permissions and limitations under the License.
 """
+from settings import APP_CODE, APP_SECRET, MAIN_DOMAIN
 
 USER_PAGE_SIZE = 20
 REQUEST_PARAMS = {
-    "bk_app_code": "bk-course-manage",
-    "bk_app_secret": "svgZF9p20lKI86YtMrAcI4WwDGyet3KcgVH8",
-    "access_token": "bkcrypt%24gAAAAABiWMn-NXrNu4z_RfBhpM1H5Lx5_NNX5uhFbEOfOEcrKm1bZniwEACUpT"
-    "-gslOQccXL8V58-W56aJrF3kQrPdboobCIU_-4RUN4hBSuMzh8FNrspCjQ2frvGNgmsHPNDggJBrf8",
+    "bk_app_code": APP_CODE,
+    "bk_app_secret": APP_SECRET,
     "fields": ["username", "departments", "display_name", "leader"],
     "wildcard_search_fields": ["departments__name", "display_name", "username"],
     "page_size": USER_PAGE_SIZE,
 }
 
-PROFILES_LIST_URL = (
-    "https://bkapi.paas-edu.bktencent.com/api/bk-user/prod/api/v2/profiles/"
-)
+PROFILES_LIST_URL = MAIN_DOMAIN + "/api/bk-user/prod/api/v2/profiles/"
