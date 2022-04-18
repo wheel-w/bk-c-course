2970fde28203ab4a022241a381e972c45dea3af7
Kala Wu <92995653+YichenWu11@users.noreply.github.com>
2022-04-18 20:58:30 +0800

    minor: 接口前缀统一改为api/ (#32)

--- project_task/urls.py
+++ project_task/urls.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版（BLUEKing PaaS Community
 Edition) available.
@@ -10,20 +9,3 @@ Unless required by applicable Law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific Language governing permissions and limitations under the License.
 """
-
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-
-from user_manager import views
-
-router = DefaultRouter()
-router.register("list", views.UserView)
-# router.register("add_tag", views.AddTagToUserView)
-# router.register("tag", views.UserTagView)
-router.register("update", views.UserUpdateView)
-router.register("account", views.AccountView)
-router.register("", views.OriginAccountView)
-router.register("batch", views.BatchView)
-urlpatterns = [
-    path("user/", include(router.urls)),
-]
