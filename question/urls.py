5d1533186ff3106fc78785a0e4c7d326fad1637e
Kala Wu <92995653+YichenWu11@users.noreply.github.com>
2022-04-18 22:28:03 +0800

    minor: add urlpatterns (#33)

--- question/urls.py
+++ question/urls.py
@@ -9,3 +9,5 @@ Unless required by applicable Law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific Language governing permissions and limitations under the License.
 """
+
+urlpatterns = []
